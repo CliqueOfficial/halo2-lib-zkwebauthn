29bb568be77c0595a67f392f59f320a5c9c194db
Richard Liu <richard.y.liu@berkeley.edu>
2023-04-02 19:28:22 +0700

    add secp256r1 ecdsa

---
+++ halo2-ecc/src/secp256r1/mod.rs
@@ -0,0 +1,14 @@
+use crate::halo2_proofs::halo2curves::secp256r1::Fp;
+
+use crate::ecc;
+use crate::fields::fp;
+
+#[allow(dead_code)]
+type FpChip<F> = fp::FpConfig<F, Fp>;
+#[allow(dead_code)]
+type Secp256r1Chip<F> = ecc::EccChip<F, FpChip<F>>;
+#[allow(dead_code)]
+const SECP_B: u64 = 7;
+
+#[cfg(test)]
+mod tests;
--- hashes/zkevm-keccak/src/lib.rs
+++ hashes/zkevm-keccak/src/lib.rs
@@ -1,11 +1,11 @@
 //! The zkEVM keccak circuit implementation, with some minor modifications
 //! Credit goes to https://github.com/privacy-scaling-explorations/zkevm-circuits/tree/main/zkevm-circuits/src/keccak_circuit
 
-use halo2_base::halo2_proofs;
-
+#[cfg(feature = "halo2-axiom")]
 /// Keccak packed multi
 pub mod keccak_packed_multi;
+#[cfg(feature = "halo2-axiom")]
 /// Util
 pub mod util;
-
+#[cfg(feature = "halo2-axiom")]
 pub use keccak_packed_multi::KeccakCircuitConfig as KeccakConfig;
